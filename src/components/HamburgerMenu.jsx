775073ea44229cbfaa3a99bab5eddc7117914f65
Anzz <makrufmustofa79@gmail.com>
2023-11-14 11:38:29 +0700

    fix design

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -7,7 +7,7 @@ const HamburgerMenu = () => {
   return (
     <>
     <div className="relative top-12 flex flex-col justify-center items-center bg-slate-200 text-red-500 h-full w-50 p-4">
-      <div className='flex flex-grow justify-between items-center gap-4'>
+      <div className='flex flex-grow justify-between items-center gap-4 pt-2'>
         <Link to={'/'} className='hover:font-semibold'><FontAwesomeIcon icon={faHouse}/>{" "}Home</Link>
         <Link to={'/preview'} className='hover:font-semibold'><FontAwesomeIcon icon={faLayerGroup}/>{" "}Produk</Link>
         <Link to={'/features'} className='hover:font-semibold'><FontAwesomeIcon icon={faStar}/>{" "}Fitur</Link>
